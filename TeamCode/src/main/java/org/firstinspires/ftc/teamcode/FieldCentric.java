7545c9dc3833d8c96597393c24ff903ffada1b2b
Samuel Wan <wanule123@gmail.com>
2025-07-02 17:27:38 -0700

    normal progress i managed to make field centric work! with backward and forward!!:+1: :+1: :+1:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentric.java
@@ -25,30 +25,8 @@ public class FieldCentric extends LinearOpMode {
     // The IMU sensor object
     GoBildaPinpointDriver odo; // Declare OpMode member for the Odometry Computer
 
-    double leftFrontPower;
-    double leftBackPower;
-    double rightFrontPower;
-    double rightBackPower;
-
-    /**
-     * This OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
-     * This code will work with either a Mecanum-Drive or an X-Drive train.
-     * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
-     *
-     * Also note that it is critical to set the correct rotation direction for each motor. See details below.
-     *
-     * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
-     * Each motion axis is controlled by one Joystick axis.
-     *
-     * 1) Axial -- Driving forward and backward -- Left-joystick Forward/Backward
-     * 2) Lateral -- Strafing right and left -- Left-joystick Right and Left
-     * 3) Yaw -- Rotating Clockwise and counter clockwise -- Right-joystick Right and Left
-     *
-     * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
-     * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
-     * the direction of all 4 motors (see code below).
-     */
     @Override
+
     public void runOpMode() {
         double axial;
         double lateral;
@@ -81,23 +59,27 @@ public class FieldCentric extends LinearOpMode {
         FRMotor.setDirection(DcMotor.Direction.FORWARD);
         BRMotor.setDirection(DcMotor.Direction.FORWARD);
 
-
-        // Recalibrate IMU
-        odo.recalibrateIMU();
-        odo.resetPosAndIMU();
-
-        // Wait for the game to start (driver presses START)
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
         waitForStart();
 
-        // Run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
+            double power;
             if (gamepad1.a) {
                 odo.resetPosAndIMU();
             }
+            power = gamepad1.right_stick_x;
+            FLMotor.setPower(power);
 
+            telemetry.addData("Encoder Position", FLMotor.getCurrentPosition());
+            BLMotor.setPower(power);
+
+            telemetry.addData("Encoder Position", BLMotor.getCurrentPosition());
+            FRMotor.setPower(power);
+
+            telemetry.addData("Encoder Position", FRMotor.getCurrentPosition());
+            BRMotor.setPower(power);
+
+            telemetry.addData("Encoder Position", BRMotor.getCurrentPosition());
+            telemetry.update();
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
             // Note: pushing stick forward gives negative value
             // stick Y direction: up is -, down is +
@@ -106,37 +88,9 @@ public class FieldCentric extends LinearOpMode {
             lateral = gamepad1.left_stick_x; // Strafe left/right (positive is right, negation is left)
             yaw = gamepad1.right_stick_x; // Turn left/rigth (positive is clockwise, negative is counter-clockwise)
 
-            odo.update();
-            double botHeading = -odo.getHeading(AngleUnit.RADIANS); // Get the robot's heading in radians
-            telemetry.addLine("botHeading " + botHeading);
-
-            // Rotate the movement direction counter to the bot's rotation
-            double rotX = lateral * Math.cos(botHeading) - axial * Math.sin(botHeading);
-            double rotY = lateral * Math.sin(botHeading) + axial * Math.cos(botHeading);
-            rotX = rotX * 1.1; //counteract imperfect strafing
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double speed = 1.7;
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(yaw), 1) * speed; //Multiply by 1.7 to reduce speed
-            leftFrontPower = (rotY + rotX + yaw) / denominator;
-            rightFrontPower = (rotY - rotX - yaw) / denominator;
-            leftBackPower = (rotY - rotX + yaw) / denominator;
-            rightBackPower = (rotY + rotX - yaw) / denominator;
-
-            // Send calculated power to wheels.
-            FLMotor.setPower(leftFrontPower);
-            FRMotor.setPower(rightFrontPower);
-            BLMotor.setPower(leftBackPower);
-            BRMotor.setPower(rightBackPower);
-
-            // Show the elapsed game time and wheel power.
-            //telemetry.addData("Status", "Run Time: " + runtime);
-            telemetry.addData("Front left/Right", JavaUtil.formatNumber(leftFrontPower, 4, 2) + ", " + JavaUtil.formatNumber(rightFrontPower, 4, 2));
-            telemetry.addData("Back  left/Right", JavaUtil.formatNumber(leftBackPower, 4, 2) + ", " + JavaUtil.formatNumber(rightBackPower, 4, 2));
+            telemetry.addData("Encoder Position", BLMotor.getCurrentPosition());
             telemetry.update();
-
         }
     }
+
 }
