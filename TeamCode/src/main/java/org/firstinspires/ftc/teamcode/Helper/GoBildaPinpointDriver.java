b116c7e23072292e83a677f8848d71037974dadd
alaqmarp <alaqmar0001@gmail.com>
2025-07-11 10:29:05 +0530

    Pinpoint

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/GoBildaPinpointDriver.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+import org.firstinspires.ftc.teamcode.Helper.Chassis;
+
+@Autonomous(name = "AlaqmarAuto", group = "Auto")
+public class Autonomus11 extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        Chassis chassis = new Chassis();
+        chassis.setDriveMode(Chassis.DriveMode.FIELD_CENTRIC);
+        chassis.init(this);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        // Move chassis autonomously
+        double maxPower = 0.5;
+        double timeoutSeconds = 10;
+        // Go in a triangle.  Do NOT change this code when you are submitting your homework
+        chassis.goToPosition(
+                new Pose2D(DistanceUnit.CM, 10, 0, AngleUnit.DEGREES, 90),
+                maxPower, timeoutSeconds);
+        chassis.goToPosition(
+                new Pose2D(DistanceUnit.CM, 10, 10, AngleUnit.DEGREES, 0),
+                maxPower, timeoutSeconds);
+        chassis.goToPosition(
+                new Pose2D(DistanceUnit.CM, 0, 0, AngleUnit.DEGREES, -45),
+                maxPower, timeoutSeconds);
+    }
+}
