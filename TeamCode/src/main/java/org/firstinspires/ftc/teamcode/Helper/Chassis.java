e365f3b4e5d0fe71f13b15c7c253927fbb19f2ed
soph88888888 <sl.bunny007@gmail.com>
2025-07-11 12:31:54 -0700

    Update Chassis.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Chassis.java
@@ -158,7 +158,7 @@ public class Chassis {
             // Get the current heading in Radians.
             double xCurrentCM = odo.getPosX(DistanceUnit.CM);
             double yCurrentCM = odo.getPosY(DistanceUnit.CM);
-            double headingCurrentRad = -odo.getHeading(AngleUnit.RADIANS);
+            double headingCurrentRad = odo.getHeading(AngleUnit.RADIANS);
 
             // Step 4c: Calculate the "error" (how far off you are) for X, Y, and Heading.
             //   - Calculate `dx` (difference in X between target and current).
