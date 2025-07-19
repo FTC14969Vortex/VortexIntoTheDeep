f599a0382e70bfc2e52322c5282a92a6b3ae1285
etloorob <etloorob@gmail.com>
2025-07-19 00:41:51 -0700

    Changed SampleAuto.java to goToPosition to 6 different positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAuto.java
@@ -26,13 +26,22 @@ public class SampleAuto extends LinearOpMode {
         double timeoutSeconds = 10;
         // Go in a triangle.  Do NOT change this code when you are submitting your homework
         chassis.goToPosition(
-                new Pose2D(DistanceUnit.CM, 10, 0, AngleUnit.DEGREES, 90),
+                new Pose2D(DistanceUnit.INCH, 0, 24, AngleUnit.DEGREES, 0),
                 maxPower, timeoutSeconds);
         chassis.goToPosition(
-                new Pose2D(DistanceUnit.CM, 10, 10, AngleUnit.DEGREES, 0),
+                new Pose2D(DistanceUnit.INCH, -24, 24, AngleUnit.DEGREES, -90),
                 maxPower, timeoutSeconds);
         chassis.goToPosition(
-                new Pose2D(DistanceUnit.CM, 0, 0, AngleUnit.DEGREES, -45),
+                new Pose2D(DistanceUnit.INCH, -24, 48, AngleUnit.DEGREES, 0),
+                maxPower, timeoutSeconds);
+        chassis.goToPosition(
+                new Pose2D(DistanceUnit.INCH, -48, 48, AngleUnit.DEGREES, -90),
+                maxPower, timeoutSeconds);
+        chassis.goToPosition(
+                new Pose2D(DistanceUnit.INCH, -48, 72, AngleUnit.DEGREES, 0),
+                maxPower, timeoutSeconds);
+        chassis.goToPosition(
+                new Pose2D(DistanceUnit.INCH, -72, 72, AngleUnit.DEGREES, -90),
                 maxPower, timeoutSeconds);
     }
 }
