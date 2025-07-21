63cc4437e7107c1faa6b4fb86dd49716e0333faa
MollyChen11 <mollyyhchen@gmail.com>
2025-07-21 15:58:15 -0700

    edited code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAuto.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
 import org.firstinspires.ftc.teamcode.Helper.Chassis;
 
-@Autonomous(name = "SampleAuto_Mecanum", group = "Auto")
+@Autonomous(name = "SampleAuto_Mecanum_Molly", group = "Auto")
 public class SampleAuto extends LinearOpMode {
     @Override
     public void runOpMode() {
@@ -26,10 +26,10 @@ public class SampleAuto extends LinearOpMode {
         double timeoutSeconds = 10;
         // Go in a triangle.  Do NOT change this code when you are submitting your homework
         chassis.goToPosition(
-                new Pose2D(DistanceUnit.CM, 10, 0, AngleUnit.DEGREES, 90),
+                new Pose2D(DistanceUnit.CM, 100, 0, AngleUnit.DEGREES, 90),
                 maxPower, timeoutSeconds);
         chassis.goToPosition(
-                new Pose2D(DistanceUnit.CM, 10, 10, AngleUnit.DEGREES, 0),
+                new Pose2D(DistanceUnit.CM, 100, 100, AngleUnit.DEGREES, 0),
                 maxPower, timeoutSeconds);
         chassis.goToPosition(
                 new Pose2D(DistanceUnit.CM, 0, 0, AngleUnit.DEGREES, -45),
