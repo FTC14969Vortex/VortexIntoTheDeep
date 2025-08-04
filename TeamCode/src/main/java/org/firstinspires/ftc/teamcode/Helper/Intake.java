e8a275a1785cc2c7dd700e3dfb1c86b2aaa0b374
Shashvat Mehta <shashvatsmehta@gmail.com>
2025-08-03 17:41:12 -0700

    Added intake and HW #14

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Intake.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Helper;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+public class Intake {
+
+    public CRServo servo;
+
+    public LinearOpMode myOpMode;
+
+    //Constructor
+    public Intake(LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+
+    public void init() throws InterruptedException {
+        servo = myOpMode.hardwareMap.get(CRServo.class, "Intake");
+        servo.setDirection(CRServo.Direction.FORWARD);
+    }
+}
