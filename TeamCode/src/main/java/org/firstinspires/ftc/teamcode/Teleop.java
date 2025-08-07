14a776f6d33c7fee664d14b29aded11242285800
Samuel Wan <wanule123@gmail.com>
2025-08-06 18:16:58 -0700

    fixed Armaan and Sophia's mistake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,4 +1,3 @@
-//imports
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
@@ -6,16 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import org.firstinspires.ftc.teamcode.SampleMechanism;
-@TeleOp(name = "TeleOp", group = "TeleOp")
-=======
->>>>>>> ce1e46cb9af3a3a02d6fdec6282ba25cfb9a7d2f
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Helper.Chassis;
@@ -33,96 +24,9 @@ public class Teleop extends LinearOpMode {
         double lateral;
         double yaw;
 
-<<<<<<< HEAD
-        motor = hardwareMap.get(DcMotor.class, "backLeftDrive");
-
-        motor.setDirection(DcMotor.Direction.FORWARD);
-=======
-=======
->>>>>>> Stashed changes
-import com.qualcomm.robotcore.util.ElapsedTime;
-=======
         robot.init(); // This initializes all hardware (chassis, arm, slider, etc.)
->>>>>>> ce1e46cb9af3a3a02d6fdec6282ba25cfb9a7d2f
 
-import org.firstinspires.ftc.robotcore.external.JavaUtil;
-
-@TeleOp(name = "TeleOp", group = "TeleOp")
-
-public class Teleop extends LinearOpMode {
-
-
-    private DcMotor frontLeftDrive;
-    private DcMotor backLeftDrive;
-    private DcMotor frontRightDrive;
-    private DcMotor backRightDrive;
-
-    double leftFrontPower;
-    double leftBackPower;
-    double rightFrontPower;
-    double rightBackPower;
-
-    /**
-     * This OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
-     * This code will work with either a Mecanum-Drive or an X-Drive train.
-     * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
-     *
-     * Also note that it is critical to set the correct rotation direction for each motor. See details below.
-     *
-     * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
-     * Each motion axis is controlled by one Joystick axis.
-     *
-     * 1) Axial -- Driving forward and backward -- Left-joystick Forward/Backward
-     * 2) Lateral -- Strafing right and left -- Left-joystick Right and Left
-     * 3) Yaw -- Rotating Clockwise and counter clockwise -- Right-joystick Right and Left
-     *
-     * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
-     * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
-     * the direction of all 4 motors (see code below).
-     */
-    @Override
-    public void runOpMode() {
-        ElapsedTime runtime;
-        float axial;
-        float lateral;
-        float yaw;
-        double max;
-
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
-        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
-        runtime = new ElapsedTime();
-        // ########################################################################################
-        // !!! IMPORTANT Drive Information. Test your motor directions. !!!!!
-        // ########################################################################################
-        //
-        // Most robots need the motors on one side to be reversed to drive forward.
-        // The motor reversals shown here are for a "direct drive" robot
-        // (the wheels turn the same direction as the motor shaft).
-        //
-        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
-        // that your motors are turning in the correct direction. So, start out with the reversals here, BUT
-        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
-        //
-        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward.
-        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
-        // <--- Click blue icon to see important note re. testing motor directions.
-        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
-        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
-        // Wait for the game to start (driver presses START)
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
         waitForStart();
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
         // Run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
@@ -130,105 +34,41 @@ public class Teleop extends LinearOpMode {
             if (gamepad1.a) {
                 robot.chassis.odo.resetPosAndIMU(); // Resets the robot’s position and heading if A is pressed
             }
-<<<<<<< HEAD
-=======
-=======
->>>>>>> Stashed changes
-        runtime.reset();
-        // Run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            // Note: pushing stick forward gives negative value
-            axial = -gamepad1.left_stick_y;
-            lateral = gamepad1.left_stick_x;
-            yaw = gamepad1.right_stick_x;
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            leftFrontPower = axial + lateral + yaw;
-            rightFrontPower = (axial - lateral) - yaw;
-            leftBackPower = (axial - lateral) + yaw;
-            rightBackPower = (axial + lateral) - yaw;
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = JavaUtil.maxOfList(JavaUtil.createListWith(Math.abs(leftFrontPower), Math.abs(rightFrontPower), Math.abs(leftBackPower), Math.abs(rightBackPower)));
-            if (max > 1) {
-                leftFrontPower = leftFrontPower / max;
-                rightFrontPower = rightFrontPower / max;
-                leftBackPower = leftBackPower / max;
-                rightBackPower = rightBackPower / max;
-            }
-            // Send calculated power to wheels.
-            frontLeftDrive.setPower(leftFrontPower);
-            frontRightDrive.setPower(rightFrontPower);
-            backLeftDrive.setPower(leftBackPower);
-            backRightDrive.setPower(rightBackPower);
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("SL Status", "Run Time: " + runtime);
-            telemetry.addData("Front left/Right", JavaUtil.formatNumber(leftFrontPower, 4, 2) + ", " + JavaUtil.formatNumber(rightFrontPower, 4, 2));
-            telemetry.addData("Back  left/Right", JavaUtil.formatNumber(leftBackPower, 4, 2) + ", " + JavaUtil.formatNumber(rightBackPower, 4, 2));
-            telemetry.update();
-        }
->>>>>>> Stashed changes
-    }
 
-    /**
-     * This function is used to test your motor directions.
-     *
-     * Each button should make the corresponding motor run FORWARD.
-     *
-     *   1) First get all the motors to take to correct positions on the robot
-     *      by adjusting your Robot Configuration if necessary.
-     *
-     *   2) Then make sure they run in the correct direction by modifying the
-     *      the setDirection() calls above.
-     */
-    private void testMotorDirections() {
-        leftFrontPower = gamepad1.x ? 1 : 0;
-        leftBackPower = gamepad1.a ? 1 : 0;
-        rightFrontPower = gamepad1.y ? 1 : 0;
-        rightBackPower = gamepad1.b ? 1 : 0;
-    }
+            // ----------------------------- ARM & SLIDER CODE -----------------------------
 
-    /**
-     * This function is used to test your motor directions.
-     *
-     * Each button should make the corresponding motor run FORWARD.
-     *
-     *   1) First get all the motors to take to correct positions on the robot
-     *      by adjusting your Robot Configuration if necessary.
-     *
-     *   2) Then make sure they run in the correct direction by modifying the
-     *      the setDirection() calls above.
-     */
-    private void testMotorDirections() {
-        leftFrontPower = gamepad1.x ? 1 : 0;
-        leftBackPower = gamepad1.a ? 1 : 0;
-        rightFrontPower = gamepad1.y ? 1 : 0;
-        rightBackPower = gamepad1.b ? 1 : 0;
-=======
+            // Set motors to run without encoders
+            robot.arm.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            robot.slider.motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-            // ----------------------------- STUDENT SECTION START -----------------------------
+            // Read input from gamepad2
+            double arm_power = gamepad2.left_stick_y;
+            double slider_power = gamepad2.right_stick_y;
 
-            // STEP 1: Set the arm and slider motors to RUN_WITHOUT_ENCODER mode.
-            // This tells the motors to respond directly to power input without trying to go to a position.
-            // Use the setMode() method and the DcMotor.RunMode.RUN_WITHOUT_ENCODER constant.
-            //To access the arm motor, use robot.arm.motor
+            // Set motor power
+            robot.arm.motor.setPower(arm_power);
+            robot.slider.motor.setPower(slider_power);
 
-            // STEP 2: Use the left joystick on gamepad2 to control the ARM.
-            // Get the vertical value of the left stick (Y-axis). Up should be positive, down negative.
-            // Save this value in a variable called `arm_power`.
+            // Telemetry output
+            telemetry.addData("arm power", arm_power);
+            telemetry.addData("slider power", slider_power);
 
-            // STEP 3: Use the right joystick on gamepad2 to control the SLIDER.
-            // Get the vertical value of the right stick (Y-axis). Up should be positive, down negative.
-            // Save this value in a variable called `slider_power`.
+            // ----------------------------- WRIST & INTAKE -----------------------------
 
+            // WRIST CONTROL:
+            // Use right_stick_x on gamepad2 to rotate the wrist slightly left/right.
+            // 1. Get the current wrist position using robot.wrist.servo.getPosition()
+            // 2. Add the gamepad joystick value to that current position value. You may need to multiply by a small scale factor.
+            // 3. Call goToPosition on the wrist to move it to the gamepad + current value.
+            //
+            // BONUS: If gamepad2.a is pressed, reset the wrist to position 0.
 
-            // STEP 5: Display the values of `arm_power` and `slider_power`
-            // on the telemetry so you can see them on the driver station.
-            // Use telemetry.addData("label", value);
+            // INTAKE CONTROL:
+            // - gamepad2 left_bumper → reverse intake (power = -1)
+            // - gamepad2 right_bumper → forward intake (power = 1)
+            // - gamepad2 x → stop intake (power = 0)
 
-
-            // ------------------------------ STUDENT SECTION END ------------------------------
+            // ----------------------------------------------------------------------------------
 
             robot.chassis.setDriveMode(Chassis.DriveMode.FIELD_CENTRIC);
 
@@ -238,8 +78,7 @@ public class Teleop extends LinearOpMode {
 
             robot.chassis.drive(axial, lateral, yaw);
 
-            telemetry.update(); // Don't forget: telemetry won't show anything unless you call update()!
+            telemetry.update(); // Required to display telemetry values
         }
->>>>>>> ce1e46cb9af3a3a02d6fdec6282ba25cfb9a7d2f
     }
 }
