52d5f3132fecd1d6e8576bff42a5f041bb43e88e
alaqmarp <alaqmar0001@gmail.com>
2025-08-15 17:23:26 -0700

    Update Chassis.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Chassis.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
