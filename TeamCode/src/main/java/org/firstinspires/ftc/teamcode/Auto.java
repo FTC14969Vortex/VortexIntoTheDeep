ab9740be3ad69a6cb875d6eed27b4a76ae410b26
alaqmarp <alaqmar0001@gmail.com>
2025-09-20 18:48:10 -0700

    a

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.helper.Chassis;
+import org.firstinspires.ftc.teamcode.helper.FlyWheel;
+import org.firstinspires.ftc.teamcode.helper.Gate;
+
+@Autonomous(name = "DecodeAutonomousV1", group = "Autonomous")
+
+public abstract class Auto extends LinearOpMode{
+
+    Chassis chassis;
+
+
+
+
+
+
+}
